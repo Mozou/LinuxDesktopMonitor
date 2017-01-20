2e0ed684a48d784763c28bf311615f57c32a4be6
ubuntu <mozou5@outlook.com>
2017-01-20 06:36:08 -0800

    define cpu info struct

--- cpu_monitor.c
+++ cpu_monitor.c
@@ -3,3 +3,22 @@
 //
 
 #include "cpu_monitor.h"
+
+int get_cpu_occupied(CPU_OCCUPY *cpu)
+{
+    FILE *file;
+    char buff[256];
+
+    file = fopen("/proc/stat", "r");
+    if(file == NULL){
+        fprintf(stderr, "open file error");
+        return -1;
+    }
+
+    fgets(buff, sizeof(buff), file);
+
+    sscanf(buff, "%s %u %u %u", cpu->name, &cpu->user, &cpu->nice, &cpu->system);
+
+    fclose(file);
+    return 0;
+}
--- cpu_monitor.h
+++ cpu_monitor.h
@@ -2,7 +2,31 @@
 // Created by hu on 1/20/17.
 //
 
-#ifndef LINUXDESKTOPMONITOR_CPU_MONITOR_H
-#define LINUXDESKTOPMONITOR_CPU_MONITOR_H
+#ifndef CPU_MONITOR_H
+#define CPU_MONITOR_H
 
-#endif //LINUXDESKTOPMONITOR_CPU_MONITOR_H
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct data{
+    char name[10];
+    unsigned int user;//    cpu_stat's user field
+    unsigned int nice;//    cpu_stat's nice field
+    unsigned int system;//  cpu_stat's sys field
+    unsigned int idle;//    cpu_stat's idle field
+    unsigned int irq;//     cpu_stat's irq field
+    unsigned int softirq;//  cpu_stat's softirq field
+}CPU_OCCUPY;
+
+typedef struct{
+    unsigned int processer_num;
+    char name[64];
+    float cpu_MHz;
+}CPU_INFO;
+
+int get_cpu_occupied(CPU_OCCUPY *cpu);
+
+unsigned int get_cpu_num();
+
+#endif //CPU_MONITOR_H
